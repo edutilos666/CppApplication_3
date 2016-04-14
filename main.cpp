28334e6a81165420a47924de552d879315470fcd
edutilos <edutilosaghayev@yahoo.com>
2016-04-14 19:36:33 +0200

    s

--- main.cpp
+++ main.cpp
@@ -12,14 +12,14 @@
  */
 
 #include <cstdlib>
-
+#include <iostream>
 using namespace std;
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-
+    cout<< "hello world" << endl ; 
     return 0;
 }
 
